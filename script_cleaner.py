009a378ca0fa60b98226b2b8fafdba8bce59356b
spencer <spencer.strickland@wsu.edu>
2019-03-31 22:46:31 -0700

    Filtered the data more heavily, only cartman lines

--- script_cleaner.py
+++ script_cleaner.py
@@ -2,6 +2,7 @@
 ## It will then pre-process my data for the RNN
 
 import re
+import operator
 
 ## Change data from CARTMAN \n screw you guys
 ## To cartman: screw you guys
@@ -18,23 +19,63 @@ script_lines = new_text.split("\n")
 
 # Remove any lines from our text we didn't process
 filtered_script = ''
+# Also count up the character
+characters = {}
 for line in script_lines:
     if '::' in line:
         line = line.replace('::',':')
     if ':' in line:
         filtered_script += line[1:]+"\n"
+        character = line.split(":")[0]
+        if character in characters: characters[character] = characters[character] + 1
+        else: characters[character] = 0
 
 
 
+# Now, we want to only grab the top 10 most common characters to help our data set
+sorted_chars = sorted(characters.items(), key=operator.itemgetter(1))
 
-# Save clean data
-clean_data = open('clean_data.txt','w',encoding='utf-8')
-clean_data.write(filtered_script)
-clean_data.close()
+# Top 10 characters in a list
+popular_chars = sorted_chars[-10:]
+top10 = []
+for char in popular_chars:
+    top10.append(char[0][1:])
 
+# Split up the lines so far
+# apply a second filter (If you are the most popular character)
+filtered_lines = filtered_script.split("\n")
+second_filter = ''
+for line in filtered_lines:
+    split = line.split(":")
+    if split[0] == top10[9]:
+        second_filter += split[1] +"\n"
+
+
+# Apply a third filter, remove all the [emotes] from the data set
+third_filter=re.sub(r'\[.+\]', r'', second_filter)
+# print(third_filter)
+
+# Now, remove all weird double newlines and start of sentence spaces
+third_filter_lines = third_filter.split("\n")
+fourth_filter = ''
+for line in third_filter_lines:
+    if len(line) > 3:
+        line.replace("\n","")
+        line  = line.strip()
+        spaceCount = 0
+        for char in line:
+            if char == " ":
+                spaceCount += 1
+            else:
+                break
+        line = line[spaceCount:]
+        fourth_filter += line + "\n"
+
+# print(fourth_filter)
 # Convert from UTF8 to ascii (data loss expected)
 # Save clean data in ascii
-ascii_data = filtered_script.encode("ascii","ignore")
-clean_data = open('clean_data_ASCII.txt','w')
+ascii_data = fourth_filter.encode("ascii", "ignore")
+clean_data = open('cartman_data.txt', 'w')
 clean_data.write(ascii_data.decode("ascii"))
 clean_data.close()
+
